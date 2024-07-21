24a6422a7afb8ca14130cda468a14e211fddf174
aidyninho <ryzyk.aidyn@gmail.com>
2024-07-22 00:31:10 +0500

    initial commit

---
+++ src/test/java/com/andersen/tickettoride/TicketToRideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andersen.tickettoride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketToRideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
